5f0d68c57f1a09b2fc2bd6f0496191b5a8a089e3
anonymousek2 <57250017+anonymousek2@users.noreply.github.com>
2020-11-18 19:22:14 +0100

    🆕 First push

---
+++ 1.main.bat
@@ -0,0 +1,167 @@
+::SETUP
+@echo off
+chcp 65001
+@title MR.ROBOT_game
+color B
+cls
+
+::Welcome
+echo XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+echo XXXXXXXXXXX Welcome to MR.ROBOT lidl edition - bat file game   XXXXXXXXXXXXXXX
+echo XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+pause
+::warning
+echo 1. this "game" contains bad ennglish
+echo 2. Is borring
+echo 3. Have 3mins of gameplay :D
+
+:: Tutorial
+:: music settings
+echo xxxxxxxxxxxxxxxxxxxxxxxxxxxx
+echo Tutorial
+echo phone =  folder of this game 
+echo xxxxxxxxxxxxxxxxxxxxxxxxxxxx
+pause
+choice /C:yn /M "Du you want music?"
+if errorlevel == 2 goto audioNO
+if errorlevel == 1 goto audioYES
+
+:audioYES
+echo audio ON
+echo To turn of music hardway use task manager and find
+
+set "file=soudtrack.mp3"
+( echo Set Sound = CreateObject("WMPlayer.OCX.7"^)
+  echo Sound.URL = "%file%"
+  echo Sound.Controls.play
+  echo do while Sound.currentmedia.duration = 0
+  echo wscript.sleep 100
+  echo loop
+  echo wscript.sleep (int(Sound.currentmedia.duration^)+1^)*1000) >sound.vbs
+start /min sound.vbs
+
+goto areyouready
+:audioNO
+echo audio vypnute
+goto areyouready
+
+:areyouready
+choice /C:yn /M "Are you ready to start? y- yes || n- no"
+if errorlevel == 2 goto nejsem
+if errorlevel == 1 goto start
+
+:nejsem
+echo You're ready, but you just don't know it yet. :)
+pause
+
+:start
+::start setup
+mode con: cols=78 lines=78
+color 2
+cls
+::
+echo hello %username%.
+echo Go to you computer.
+
+choice /C:yn /M "Y- Go to your computer N- Stay where you are"
+if errorlevel == 2 goto oneno
+if errorlevel == 1 goto oneyes
+
+:oneno
+echo So i heard you want go to your computer :).
+pause
+cls
+
+:oneyes
+echo " ______________       "
+echo " ||            ||     "
+echo " ||            ||     "
+echo " ||            ||     "
+echo " ||            ||     "
+echo " ||____________||     "
+echo " |______________|     "
+echo " \\############\\     "
+echo "  \\############\\    "
+echo "   \      ____    \   "
+echo "    \_____\___\____\  "
+echo ________________________ 
+::docasne dodkud nebudu mít lepší nápad
+:legithAcKinG1
+echo Get inside the laptop.
+choice /C:BED /M "B- BruceForce E- use Exploit D- dump files"
+if errorlevel == 3 goto twoDumpFiles
+if errorlevel == 2 goto twoExploit
+if errorlevel == 1 goto twoBruceForce
+
+:twoExploit
+    echo You can't figure out any exploit.
+    goto legithAcKinG1
+
+:twoDumpFiles
+    echo You dump HARDdisk of computer.
+    echo Dumped files you can find at your phone
+    mkdir phone
+    echo 556c74696d61746550617373776f72643132334e4f6f6e654b4e4f5768696d >> phone/data.txt
+    echo Find that files and press any key
+    pause
+    echo Decrypt data from data.txt which you can find in your mobile.
+    echo Looks like it's encrypted in Hex Code
+    pause
+    echo after decrypting try bruteforce attack again
+    pause
+    goto legithAcKinG1
+
+:twoBruceForce
+    echo PC: Enter your password
+    set /p "pass=>"
+    if /I "%pass%"=="UltimatePassword123NOoneKNOWhim" (
+        GOTO :done
+    ) ELSE (
+        GOTO :fail
+    )
+:fail
+echo invalid password
+choice /C:tb /M "t= try again b= try different method"
+if errorlevel == 2 goto legithAcKinG1
+if errorlevel == 1 goto twoBruceForce
+    
+:done
+echo pc: Welcome back 
+echo Nice you get inside
+mkdir loot
+echo 01001000 01100101 01110010 01100101 00100000 01110011 01101000 01101111 01110101 01101100 01100100 00100000 01100010 01100101 00100000 01110011 01101111 01101101 01100101 00100000 01100100 01100001 01110100 01100001 00101100 00100000 01100010 01110101 01110100 00100000 01101001 01101101 00100000 01110100 01101111 01101111 00100000 01101100 01100001 01111010 01111001 00100000 01110100 01101111 00100000 01101101 01100001 01101011 01100101 00100000 01110100 01101000 01100101 01101101 00101110 00100000 00111010 00101001 >> loot/data.txt
+pause
+goto insidePC
+
+:insidePC
+echo now open "secret.exe"
+set /p "secret=C:\Users\root>"
+    if /I "%secret%"=="secret.exe" (
+        GOTO :openfile
+    ) ELSE (
+        GOTO :pepega
+    )
+
+:pepega
+echo try write secret.exe into cmd
+pause
+goto insidePC
+
+:openfile
+cls
+call 10.Fsociety_logo.bat
+echo You done all your task.
+pause
+echo So now you can join us.
+pause
+echo We will met here:  4d616465206279204544454d53
+
+
+::konec
+:konec
+color B
+cls
+echo This is the end of the game
+echo If you like give me star on my github
+echo github: 
+pause
---
+++ 10.Fsociety_logo.bat
@@ -0,0 +1,65 @@
+@echo off
+
+echo XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+echo XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+echo XX                                                                          XX
+echo XX   MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM   XX
+echo XX   MMMMMMMMMMMMMMMMMMMMMssssssssssssssssssssssssssMMMMMMMMMMMMMMMMMMMMM   XX
+echo XX   MMMMMMMMMMMMMMMMss'''                          '''ssMMMMMMMMMMMMMMMM   XX
+echo XX   MMMMMMMMMMMMyy''                                    ''yyMMMMMMMMMMMM   XX
+echo XX   MMMMMMMMyy''                                            ''yyMMMMMMMM   XX
+echo XX   MMMMMy''                                                    ''yMMMMM   XX
+echo XX   MMMy'                                                          'yMMM   XX
+echo XX   Mh'                                                              'hM   XX
+echo XX   -                                                                  -   XX
+echo XX                                                                          XX
+echo XX   ::                                                                ::   XX
+echo XX   MMhh.        ..hhhhhh..                      ..hhhhhh..        .hhMM   XX
+echo XX   MMMMMh   ..hhMMMMMMMMMMhh.                .hhMMMMMMMMMMhh..   hMMMMM   XX
+echo XX   ---MMM .hMMMMdd:::dMMMMMMMhh..        ..hhMMMMMMMd:::ddMMMMh. MMM---   XX
+echo XX   MMMMMM MMmm''      'mmMMMMMMMMyy.  .yyMMMMMMMMmm'      ''mmMM MMMMMM   XX
+echo XX   ---mMM ''             'mmMMMMMMMM  MMMMMMMMmm'             '' MMm---   XX
+echo XX   yyyym'    .              'mMMMMm'  'mMMMMm'              .    'myyyy   XX
+echo XX   mm''    .y'     ..yyyyy..  ''''      ''''  ..yyyyy..     'y.    ''mm   XX
+echo XX           MN    .sMMMMMMMMMss.   .    .   .ssMMMMMMMMMs.    NM           XX
+echo XX           N`    MMMMMMMMMMMMMN   M    M   NMMMMMMMMMMMMM    `N           XX
+echo XX            +  .sMNNNNNMMMMMN+   `N    N`   +NMMMMMNNNNNMs.  +            XX
+echo XX              o+++     ++++Mo    M      M    oM++++     +++o              XX
+echo XX                                oo      oo                                XX
+echo XX           oM                 oo          oo                 Mo           XX
+echo XX         oMMo                M              M                oMMo         XX
+echo XX       +MMMM                 s              s                 MMMM+       XX
+echo XX      +MMMMM+            +++NNNN+        +NNNN+++            +MMMMM+      XX
+echo XX     +MMMMMMM+       ++NNMMMMMMMMN+    +NMMMMMMMMNN++       +MMMMMMM+     XX
+echo XX     MMMMMMMMMNN+++NNMMMMMMMMMMMMMMNNNNMMMMMMMMMMMMMMNN+++NNMMMMMMMMM     XX
+echo XX     yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy     XX
+echo XX   m  yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy  m   XX
+echo XX   MMm yMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMy mMM   XX
+echo XX   MMMm .yyMMMMMMMMMMMMMMMM     MMMMMMMMMM     MMMMMMMMMMMMMMMMyy. mMMM   XX
+echo XX   MMMMd   ''''hhhhh       odddo          obbbo        hhhh''''   dMMMM   XX
+echo XX   MMMMMd             'hMMMMMMMMMMddddddMMMMMMMMMMh'             dMMMMM   XX
+echo XX   MMMMMMd              'hMMMMMMMMMMMMMMMMMMMMMMh'              dMMMMMM   XX
+echo XX   MMMMMMM-               ''ddMMMMMMMMMMMMMMdd''               -MMMMMMM   XX
+echo XX   MMMMMMMM                   '::dddddddd::'                   MMMMMMMM   XX
+echo XX   MMMMMMMM-                                                  -MMMMMMMM   XX
+echo XX   MMMMMMMMM                                                  MMMMMMMMM   XX
+echo XX   MMMMMMMMMy                                                yMMMMMMMMM   XX
+echo XX   MMMMMMMMMMy.                                            .yMMMMMMMMMM   XX
+echo XX   MMMMMMMMMMMMy.                                        .yMMMMMMMMMMMM   XX
+echo XX   MMMMMMMMMMMMMMy.                                    .yMMMMMMMMMMMMMM   XX
+echo XX   MMMMMMMMMMMMMMMMs.                                .sMMMMMMMMMMMMMMMM   XX
+echo XX   MMMMMMMMMMMMMMMMMMss.           ....           .ssMMMMMMMMMMMMMMMMMM   XX
+echo XX   MMMMMMMMMMMMMMMMMMMMNo         oNNNNo         oNMMMMMMMMMMMMMMMMMMMM   XX
+echo XX                                                                          XX
+echo XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+echo XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+echo     .o88o.                               o8o                .
+echo     888 `"                               `"'              .o8
+echo    o888oo   .oooo.o  .ooooo.   .ooooo.  oooo   .ooooo.  .o888oo oooo    ooo
+echo     888    d88  "8 d88' `88b d88' `"Y8 `888  d88' `88b   888    `88.  .8'
+echo     888    `"Y88b.  888   888 888        888  888ooo888   888     `88..8'
+echo     888    o.  88b 888   888 888   .o8  888  888    .o   888 .    `888'
+echo    o888o   8""888P' `Y8bod8P' `Y8bod8P' o888o `Y8bod8P'   "888"      d8'
+echo                                                                 .o...P'
+echo                                                                 `....'
